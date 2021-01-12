8c8e6ef71ac1f839f5d438b4bfcafd65f1318f52
lzj-0304 <lzj_0304@163.com>
2021-01-12 16:14:13 +0800

    添加v_0.1 版  环境基本配置

---
+++ src/main/java/com/lzj/crm/controller/IndexController.java
@@ -0,0 +1,37 @@
+package com.lzj.crm.controller;
+
+import com.lzj.base.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class IndexController extends BaseController {
+
+
+    /**
+     * 系统登录页
+     * @return
+     */
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+
+    // 系统界面欢迎页
+    @RequestMapping("welcome")
+    public String welcome(){
+        return "welcome";
+    }
+
+    /**
+     * 后端管理主页面
+     * @return
+     */
+    @RequestMapping("main")
+    public String main(HttpServletRequest request){
+        return "main";
+    }
+}
